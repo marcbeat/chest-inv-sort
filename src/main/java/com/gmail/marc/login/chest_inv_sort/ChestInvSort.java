46fc950d91a09ab289ce71369152533dcdc3e721
Brünninghaus, Marc <m.bruenninghaus@gmail.com>
2024-05-15 01:02:16 +0200

    first working client side sorting in chests

--- src/main/java/com/gmail/marc/login/chest_inv_sort/ChestInvSort.java
+++ src/main/java/com/gmail/marc/login/chest_inv_sort/ChestInvSort.java
@@ -1,20 +1,11 @@
 package com.gmail.marc.login.chest_inv_sort;
 
+
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,10 +15,21 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+
 import org.slf4j.Logger;
+
+import net.minecraft.client.gui.screens.inventory.ContainerScreen;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.event.ScreenEvent;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import org.lwjgl.glfw.GLFW;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ChestInvSort.MODID)
@@ -37,52 +39,24 @@ public class ChestInvSort
     public static final String MODID = "chest_inv_sort";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
-
-    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEat().nutrition(1).saturationMod(2f).build())));
-
-    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
+    
 
     public ChestInvSort()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
+        forgeEventBus.addListener(ChestInvSort::onKeyPressed);
+
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -90,19 +64,7 @@ public class ChestInvSort
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
 
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
-    }
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
+        LOGGER.info("SORT KEY >> {}", Config.sortKey);
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -122,7 +84,50 @@ public class ChestInvSort
         {
             // Some client setup code
             LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("SORT KEY >> {}", Config.sortKey);
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
+
+    private static void onKeyPressed(ScreenEvent.KeyPressed.Pre event) {
+        // Check if the currently open GUI is a container (inventory or chest)
+        LOGGER.debug("Pressed key >> {}", GLFW.glfwGetKeyName(event.getKeyCode(), event.getScanCode()));
+        if (event.getScreen() instanceof ContainerScreen) {
+            LOGGER.debug("Opened container >> {}", event.getScreen().getClass().getName());
+
+            // Check if the sort inventory key is pressed (replace GLFW.GLFW_KEY_X with the desired key code)
+            String pressedKey = GLFW.glfwGetKeyName(event.getKeyCode(), event.getScanCode());
+            if (pressedKey != null && Config.sortKey.toLowerCase().equals(pressedKey) ) {
+                LOGGER.debug("Correct key pressed! Sorting...");
+                ContainerScreen containerScreen = (ContainerScreen) event.getScreen();
+                LocalPlayer player = containerScreen.getMinecraft().player;
+                if (player == null) return;
+                
+                NonNullList<Slot> slots = player.containerMenu.slots;
+                List<ItemStack> items = new ArrayList<>(slots.size());
+                // Get all non-empty slots
+                for (int i = 0; i < slots.size(); i++) {
+                    Slot slot = slots.get(i);
+                    if (!slot.hasItem()) continue;
+                    ItemStack stack = slot.getItem();
+                    items.add(stack);
+                    LOGGER.debug("Item id in slot {} : {}", i, stack.getDescriptionId());
+                }
+                // Sort items
+                items.sort((ItemStack a, ItemStack b) -> { return a.getDisplayName().getString().compareTo(b.getDisplayName().getString()); });
+                // Put sorted items back into the inventory
+                for (int i = 0; i < slots.size(); i++) {
+                    Slot slot = slots.get(i);
+                    if (i >= items.size()) slot.set(ItemStack.EMPTY);
+                    else
+                        slot.set(items.get(i));
+                }
+            }
+        }
+    }
+    // private void onMouseButtonPressed(ScreenEvent.MouseButtonPressed.Pre event) {
+    //     if (event.getScreen() instanceof ContainerScreen) {
+            
+    //     }
+    // }
 }
